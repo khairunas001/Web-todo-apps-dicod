183995a435d78acdf7b647e7d9268ec33bb2c6e1
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2023-10-28 12:20:46 +0700

    data tersimpan pada local storage dan ditampilkan

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@
 
 const todos = [];
 const RENDER_EVENT = 'render-todo';
+const SAVED_EVENT = 'saved-todo';
+const STORAGE_KEY = 'TODO_APPS';
 
 function generateId() {
   return +new Date();
@@ -98,6 +100,7 @@ function addTodo() {
   todos.push(todoObject);
 
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 }
 
 function addTaskToCompleted(todoId /* HTMLELement */) {
@@ -106,6 +109,7 @@ function addTaskToCompleted(todoId /* HTMLELement */) {
 
   todoTarget.isCompleted = true;
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 }
 
 function removeTaskFromCompleted(todoId /* HTMLELement */) {
@@ -114,6 +118,7 @@ function removeTaskFromCompleted(todoId /* HTMLELement */) {
   todos.splice(todoTarget, 1);
 
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 }
 
 function undoTaskFromCompleted(todoId /* HTMLELement */) {
@@ -121,6 +126,36 @@ function undoTaskFromCompleted(todoId /* HTMLELement */) {
   if (todoTarget == null) return;
 
   todoTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function saveData() {
+  if (isStorageExist()) {
+    const parsed = JSON.stringify(todos);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+  }
+}
+
+function isStorageExist() /* boolean */ {
+  if (typeof (Storage) === undefined) {
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+ 
+  if (data !== null) {
+    for (const todo of data) {
+      todos.push(todo);
+    }
+  }
+ 
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
@@ -131,6 +166,10 @@ document.addEventListener('DOMContentLoaded', function () {
     event.preventDefault();
     addTodo();
   });
+  
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
 });
 
 
@@ -151,3 +190,11 @@ document.addEventListener(RENDER_EVENT, function () {
     }
   }
 });
+
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(localStorage.getItem(STORAGE_KEY));
+});
+
+
+
+ 
